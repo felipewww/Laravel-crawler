52b2aaa6d024365b7e31882903b91fb544fff137
felipewww <felipewww@outlook.com.br>
2018-09-08 15:44:56 -0300

    inserir bootstrap e alteração inicial das rotas

---
+++ app/resources/views/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap/bootstrap.min.css">
+
+        <title>Uplexis - PHP Test</title>
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
